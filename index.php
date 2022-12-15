97c0a7c674bffe5032d0744f4e5c0306a7bae092
wawrzol121 <114051655+wawrzol121@users.noreply.github.com>
2022-12-15 14:17:58 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,35 +4,20 @@
     <title>strona główna</title>
     <link rel="stylesheet" href="style.css">
 </head>
+    <body>
 <header>
 <h3> Wypożyczalnia Filmów</h3>
 </header>
-    <div class="log"><button type="button"><a href="login.php">zaloguj</a></button></div>
-<body>
-    <div class="a">
-    <?php
-$conn = new mysqli("localhost", "root", "", "move");
+   
 
-       $sql= '"SELECT idfilm, nazwa ,rodzaj   , dlugosc , rok_produkcji ,autor,swypozyczony FROM film AS i JOIN user ON id=autor.iduser"';
-         
-    $res = $conn->query("SELECT `idfilm`, `Nazwa`,`rodzaj`,`dlugosc`, `rok_produkcji`,`autor`, `wypozyczony`  FROM `film`");
-    $film = $res->fetch_all(MYSQLI_ASSOC);
-    for($i = 0; $i<count($film); $i++){
-        echo "<h3>idfilm = ".$film[$i]["idfilm"]."</h3>";
-        echo"<h3>Nazwa = ".$film[$i]["Nazwa"]."</h3>";
-        echo"<h3>rodzaj =".$film[$i]["rodzaj"]."</h3>";
-        echo"<h3>dlugosc =".$film[$i]["dlugosc"]."</h3>";
-        echo"<h3>rok_produkcji =".$film[$i]["rok_produkcji"]."</h3>";
-        echo"<h3>autor = ".$film[$i]["autor"]."</h3>";
-        echo"<h3>wypozyczony =".$film[$i]["wypozyczony"]."</h3>";
-        echo "<hr/>";
-    }
+    <div class="aa">
+        <h3>Zampraszamy do oglądania <br>
+            Zaloguj się żeby zobaczyć dostępne filmy </h3>
+        <button type="button"><a href="login.php">zaloguj</a></button><br><button type="button"><a href="register.php">zapisz się</a></button> </div>
         
-
+        
     
-
-?>
-    </div>
+    
   
     
     </body>

d25274ecc51d8f72b1192f435f68f03f9f5f2396
wawrzol121 <114051655+wawrzol121@users.noreply.github.com>
2022-12-15 14:35:03 +0100

    Create logout.php

---
+++ logout.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>strona główna</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+    <body>
+<header>
+<h3> Wypożyczalnia Filmów</h3>
+</header>
+   
+
+    <div class="a">
+        <h3>Zostałeś wylogowany</h3>
+        <button type="button"><a href="login.php">zaloguj ponownie</a></button><br></div>
+        
+        
+    
+    
+  
+    
+    </body>
+</html>
